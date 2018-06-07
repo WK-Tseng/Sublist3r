60ba76b8c9bc2aec0fb5aef04d30218da0adf289
WK-Tseng <k50402k.cs06g@nctu.edu.tw>
2018-06-07 23:52:23 +0800

    修改測資增加條件_5

--- sublist3r_UnitTest.py
+++ sublist3r_UnitTest.py
@@ -62,7 +62,7 @@ class sublist3r_UnitTest(unittest.TestCase):
         print("==============================")
 
     def test_google_com(self):
-        engines = 'Bing,Baidu,PassiveDNS,Virustotal,ThreatCrowd,Netcraft,Error'
+        engines = 'Google,Yahoo,Ask,SSL,Bing,Baidu,PassiveDNS,Virustotal,ThreatCrowd,DNSdumpster,Netcraft,Error'
         domain = 'google.com'
         ports = None
         subdomains = sublist3r.main(domain, self.no_threads, self.savefile, ports, silent=False, verbose=True, enable_bruteforce=self.enable_bruteforce, engines=engines)
